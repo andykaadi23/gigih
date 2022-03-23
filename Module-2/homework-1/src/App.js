6445c611fb96c42944355799cf33ae7340c99e1d
Andyka Adi <andykaadi23@gmail.com>
2022-03-23 19:55:42 +0700

    menambah module2 homework1

---
+++ Module-2/homework-1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Track Info</h1>
+      <div class="music">
+        <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" />
+        <h2>Bohemian Rhapsody</h2>
+        <p>Queen</p>
+        <button>Select</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
