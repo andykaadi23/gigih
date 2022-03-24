6da26c3754bad25fccd8a8b09faf0c1df9136af0
Andyka Adi <andykaadi23@gmail.com>
2022-03-24 23:07:52 +0700

    menambahkan tugas module2 homework2

--- Module-2/homework-1/src/App.js
+++ Module-2/homework-1/src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Track from './components/track';
 
 function App() {
   return (
     <div className="App">
-      <h1>Track Info</h1>
-      <div class="music">
-        <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" />
-        <h2>Bohemian Rhapsody</h2>
-        <p>Queen</p>
-        <button>Select</button>
-      </div>
+      <Track />
     </div>
   );
 }
---
+++ Module-2/homework-1/src/components/track.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Track() {
+  return (
+    <>
+      <h1>Track Info</h1>
+      <div class="music">
+        <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" />
+        <h2>Bohemian Rhapsody</h2>
+        <p>Queen</p>
+        <button>Select</button>
+      </div>
+    </>
+  );
+}
+
+export default Track;
